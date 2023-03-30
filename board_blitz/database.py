9259fabdd44277c0c2ed63e3f16f17aca3ff9413
Karinkraemer <128590740+Karinkraemer@users.noreply.github.com>
2023-03-30 14:44:50 +0200

    Connect to DB, create Login and scores tables

--- board_blitz/database.py
+++ board_blitz/database.py
@@ -1,3 +1,35 @@
 class database:
     def __init__(self) -> None:
         pass
+
+import sqlite3
+
+connection = sqlite3.connect('gamesdb.db')
+cursor = connection.cursor()
+
+sql_table_login ="""
+    CREATE TABLE IF NOT EXISTS Login (
+    pid_login INTEGER PRIMARY KEY,
+    username VARCHAR(30),     
+    pwhash VARCHAR(30),
+    pwsalt VARCHAR(16)       
+    );""" 
+cursor.execute(sql_table_login)
+
+#PLAIN TEXT USER NAME; SALTED HASH FOR THE PASSWORD
+
+sql_table_scores ="""
+    CREATE TABLE IF NOT EXISTS scores (
+    pid INTEGER,
+    username VARCHAR(30),
+    gamename VARCHAR(30),
+    score INT,
+    difficulty VARCHAR(30)    
+    );"""
+cursor.execute(sql_table_scores)
+
+
+connection.commit()
+connection.close()
+
+    
