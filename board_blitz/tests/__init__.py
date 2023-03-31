d38c59601a98b2d5ba78c946f2075ed9f87e6cd0
Nils Baumann <112630214+Nils-AB@users.noreply.github.com>
2023-03-31 10:21:44 +0200

    add registration test and refactor

--- board_blitz/tests/__init__.py
+++ board_blitz/tests/__init__.py
@@ -1,4 +1,4 @@
 # tests can be started using
 # $ cd board_blitz
 # $ python -m unittest tests
-from tests.test_menu_logic import TestLogin
+from tests.test_menu_logic import *
--- board_blitz/tests/test_menu_logic.py
+++ board_blitz/tests/test_menu_logic.py
@@ -9,38 +9,74 @@ class TestLogin(unittest.TestCase):
             "salt": "4c7d9b6418c49f35f9371805af57f396",
             "password": "4e857af91d0359157ead176cae92fea42d693f1ee7f1f79ec2e1ed2d9b89077e"
         }]
-    
-    @patch("database.get_users")
-    def test_missing_username(self, mocked_get_users):
-        mocked_get_users.return_value = self.users
-        menu_logic: MenuLogic = MenuLogic()
-        err = menu_logic.login("", "test")
+    menu_logic: MenuLogic = MenuLogic()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_get_users = patch('database.get_users')
+        cls.mock_get_users.start().return_value = cls.users
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mock_get_users.stop()
+
+    def test_missing_username(self):
+        err = self.menu_logic.login("", "test")
         self.assertIsInstance(err, str)
-    
-    @patch("database.get_users")
-    def test_missing_password(self, mocked_get_users):
-        mocked_get_users.return_value = self.users
-        menu_logic: MenuLogic = MenuLogic()
-        err = menu_logic.login("Susi", "")
+
+    def test_missing_password(self):
+        err = self.menu_logic.login("Susi", "")
         self.assertIsInstance(err, str)
-    
-    @patch("database.get_users")
-    def test_missing_user(self, mocked_get_users):
-        mocked_get_users.return_value = self.users
-        menu_logic: MenuLogic = MenuLogic()
-        err = menu_logic.login("Sabine", "hehehe")
+
+    def test_missing_user(self):
+        err = self.menu_logic.login("Sabine", "hehehe")
         self.assertIsInstance(err, str)
-    
-    @patch("database.get_users")
-    def test_wrong_password(self, mocked_get_users):
-        mocked_get_users.return_value = self.users
-        menu_logic: MenuLogic = MenuLogic()
-        err = menu_logic.login("Susi", "not_lmao")
+
+    def test_wrong_password(self):
+        err = self.menu_logic.login("Susi", "not_lmao")
         self.assertIsInstance(err, str)
-    
-    @patch("database.get_users")
-    def test_succeed(self, mocked_get_users):
-        mocked_get_users.return_value = self.users
-        menu_logic: MenuLogic = MenuLogic()
-        err = menu_logic.login("Susi", "lmao")
+
+    def test_succeed(self):
+        err = self.menu_logic.login("Susi", "lmao")
+        self.assertIsNone(err)
+
+class TestRegister(unittest.TestCase):
+    users = [{
+            "name": "Susi", # pw: "lmao"
+            "user_id": "2",
+            "salt": "4c7d9b6418c49f35f9371805af57f396",
+            "password": "4e857af91d0359157ead176cae92fea42d693f1ee7f1f79ec2e1ed2d9b89077e"
+        }]
+    menu_logic: MenuLogic = MenuLogic()
+
+    @classmethod
+    def setUpClass(cls):
+        cls.mock_get_users = patch('database.get_users')
+        cls.mock_get_users.start().return_value = cls.users
+        cls.mock_add_user = patch('database.add_user')
+        cls.mock_add_user.start().return_value = True
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.mock_get_users.stop()
+        cls.mock_add_user.stop()
+
+    def test_missing_username(self):
+        err = self.menu_logic.register("", "test", "test")
+        self.assertIsInstance(err, str)
+
+    def test_missing_password(self):
+        err = self.menu_logic.register("Fredrick", "", "")
+        self.assertIsInstance(err, str)
+
+    def test_existing_user(self):
+        err = self.menu_logic.register("Susi", "a_pw", "a_pw")
+        self.assertIsInstance(err, str)
+
+    def test_unmatching_password(self):
+        err = self.menu_logic.register("Kurt", "password", "a_different_password")
+        self.assertIsInstance(err, str)
+
+    def test_succeed(self):
+        err = self.menu_logic.register("Ben", "supersecret", "supersecret")
         self.assertIsNone(err)
