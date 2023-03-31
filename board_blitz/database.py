6572081abcb2538581e1cabbabc0e3b09404ce7f
Karinkraemer <128590740+Karinkraemer@users.noreply.github.com>
2023-03-31 12:43:18 +0200

    Added functions for Leaderbord add and show
    Also changed score points for status won/lost

--- board_blitz/database.py
+++ board_blitz/database.py
@@ -1,7 +1,6 @@
 class database:
     def __init__(self) -> None:
         pass
-
 import sqlite3
 
 connection = sqlite3.connect('gamesdb.db')
@@ -14,22 +13,45 @@ sql_table_login ="""
     pwhash VARCHAR(30),
     pwsalt VARCHAR(16)       
     );""" 
-cursor.execute(sql_table_login)
-
 #PLAIN TEXT USER NAME; SALTED HASH FOR THE PASSWORD
+cursor.execute(sql_table_login)
 
 sql_table_scores ="""
     CREATE TABLE IF NOT EXISTS scores (
     pid INTEGER,
     username VARCHAR(30),
     gamename VARCHAR(16),
-    score INT,
-    difficulty INT    
+    difficulty INT, 
+    game_won INT 
     );"""
 cursor.execute(sql_table_scores)
 
+#difficulty:  1 = leicht / 2 = mittel / 3 = schwer
+#game_won: 0 = FALSE lost / 1 = TRUE won, no boolean type in SQLite
 
 connection.commit()
 connection.close()
 
-    
+
+
+#Function to add score to the leaderboard
+# def add_leaderboard_entry(pid, difficulty, gamename, game_won):
+#   connection = sqlite3.connect('gamesdb.db')
+#   cursor = connection.cursor()
+#   cursor.execute('INSERT INTO scores(pid, Login.username, gamename, difficulty, game_won) VALUES (?, ?, ?, ?, ?)')), (pid, Login.username, gamename, difficulty, game_won))
+# connection.commit()
+# connection.close()
+
+
+
+
+# SQL command to order the Leaderboard, to be called in the GUI menu "show highscores" 
+# def get_leaderboard(difficulty):
+#   connection = sqlite3.connect('gamesdb.db')
+#   cursor = connection.cursor()
+#   cursor.execute('SELECT Login.username, scores.gamename, scores.difficulty, scores.game_won
+#   FROM Login INNER JOIN scores ON Login.pid_login = scores.pid 
+#   WHERE scores.difficulty = ? 
+#   ORDER BY scores.gamename DESC, scores.game_won DESC LIMIT 15') 
+#   return cursor.fetchall()
+# connection.close()
