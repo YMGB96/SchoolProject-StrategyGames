94782721f6d6101419fdff6b3cdd3e3ce2376dfb
LeenSamtmann <AileenS94@web.de>
2023-03-31 16:36:32 +0200

    add font display and pause rudimentary pause menu

--- board_blitz/game_gui.py
+++ board_blitz/game_gui.py
@@ -6,6 +6,8 @@ class game_gui:
     def __init__(self) -> None:
         pass
 
+pygame.font.init()
+
 testArrayInitial = [[0,2,0,2,0,2],[2,0,2,0,2,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,1,0,1,0,1],[1,0,1,0,1,0]]
 testArrayValid = [[0,2,0,2,0,2],[2,0,2,0,2,0],[0,0,0,0,0,0],[3,0,3,0,0,0],[0,1,0,1,0,1],[1,0,1,0,1,0]]
 currentArray = testArrayInitial
@@ -14,17 +16,37 @@ testPlayerImg = pygame.image.load('./board_blitz/resources/testPiece.png')
 testEnemyImg = pygame.image.load('./board_blitz/resources/testPieceEnemy.png')
 testValidMove = pygame.image.load('./board_blitz/resources/validMove.png')
 
+testButtonRect = pygame.image.load('./board_blitz/resources/validMove.png')
+
 testPiecePlayer = GamePiecePlayer(testPlayerImg)
 testEnemy = GamePiece(testEnemyImg)
 testValidMoveIndicator = GamePiecePlayer(testValidMove)
+
+testButton = GamePiecePlayer(testButtonRect)
+testButtonMenu = GamePiecePlayer(testButtonRect)
+
+screenWidth = 1040
+screenHeight = 800
 
 renderStartX = 100
 renderStartY = 70
 tileSize = 60
 
+isPaused = False
+
 isPieceClicked = False
 pieceClickedPosition = []
     
+font = pygame.font.SysFont("arialblack",20)
+textColour = (0,0,0)
+
+#functions
+
+def drawText(textToDraw, font, textColour, x, y):
+    text = font.render(textToDraw, True, textColour)
+    window.blit(text, (x,y))
+
+
 def drawBoard():
     positionBlack = [renderStartX,renderStartY,tileSize,tileSize]
     positionWhite = [tileSize*2,renderStartY,tileSize,tileSize]
@@ -76,25 +98,49 @@ def drawPieces(gameArray):
         rowNumber += 1
 
 def displayValidMoves():
-    global testArrayValid
     return testArrayValid
     # TODO: access game logic for valid moves
 
+def pauseButton():
+    positionPauseButton = pygame.Rect(10,10,60,60)
+    global isPaused
+    if testButton.draw(window,positionPauseButton) and not isPaused:
+        isPaused = True
+        print("Pause clicked")
+        # pygame.draw.rect(window,(0,0,0),(screenWidth,screenHeight,0,0)) # should do an overlay screen but doesn't :)
+        
+
+def drawPauseMenu():
+    positionAbortButton = pygame.Rect(30,(screenHeight/2+120),tileSize,tileSize)
+    positionMainMenuButton = pygame.Rect(30,(screenHeight/2),tileSize,tileSize)
+    positionBackButton = pygame.Rect(30,(screenHeight/2-120),tileSize,tileSize)
+    global isPaused
+    if testButtonMenu.draw(window,positionAbortButton) and isPaused == True:
+        print("Spiel aufgeben")
+    if testButtonMenu.draw(window,positionMainMenuButton) and isPaused == True:
+        print("Hauptmenü")
+    if testButtonMenu.draw(window,positionBackButton) and isPaused == True:
+        print("Zurück zum Spiel")
+        isPaused = False
+
 pygame.init()
-window = pygame.display.set_mode((1040,800))
+window = pygame.display.set_mode((screenWidth,screenHeight))
 
 background = pygame.Surface(window.get_size())
 background = background.convert()
 background.fill((150,150,150))
 window.blit(background, (0,0))
 
-
-
 run = True
 while run:
 
     drawBoard()
     drawPieces(currentArray)
+    pauseButton()
+    if isPaused:
+        drawPauseMenu()
+    drawText("Spielername",font,textColour,30,(screenHeight-30))
+    drawText("Gegner",font,textColour,(screenWidth-100),30)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
