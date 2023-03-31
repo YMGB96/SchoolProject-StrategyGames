bc61efe0cb497d5d968dc6ba2663c6bd5d4a88ae
YMGB96 <yannick.brands@gmx.net>
2023-03-31 12:03:39 +0200

    Dame logic works now
    Mainly Fixed one indentation error that messed up my entire gamelogic for the AI, added start function to set the board etc.

--- board_blitz/game_logic.py
+++ board_blitz/game_logic.py
@@ -4,28 +4,31 @@ class game_logic:
 
 import ai as ai
 import game_gui as game_gui
-import menu_logic as menu_logic
 
 
-# test case
-
-board = [[0,2,0,2,0,2],
-         [2,0,2,0,2,0],
-         [0,0,0,1,0,0],
-         [0,0,0,0,0,0],
-         [0,1,0,1,0,1],
-         [1,0,1,0,1,0]]
-
-chosen_piece = (4,1)
-#
-
-
-
-active_difficulty = menu_logic.menu_logic.active_difficulty
-active_game = menu_logic.menu_logic.active_game
-active_user = menu_logic.menu_logic.active_user
-
-
+def start(active_user, active_game, active_difficulty):
+    global user
+    global game
+    global difficulty   
+    global board 
+    user = active_user
+    game = active_game
+    difficulty = active_difficulty
+    if game == 0:
+        board = [[2,2,2,2,2,2],
+                 [0,0,0,0,0,0],
+                 [0,0,0,0,0,0],
+                 [0,0,0,0,0,0],
+                 [0,0,0,0,0,0],
+                 [1,1,1,1,1,1]]
+    elif game == 1:
+        board = [[0,2,0,2,0,2],
+                 [2,0,2,0,2,0],
+                 [0,0,0,0,0,0],
+                 [0,0,0,0,0,0],
+                 [0,1,0,1,0,1],
+                 [1,0,1,0,1,0]]
+        
 def find_all(matrix, element): #helperfunction for get_valid_moves ai version
     yield from ((row_no, col_no)   #Iterate through all row, column indexes in a 2D Matrix where matrix[row][col] == element
         for row_no, row in enumerate(matrix)  
@@ -33,10 +36,9 @@ def find_all(matrix, element): #helperfunction for get_valid_moves ai version
         if matrix_element == element)  
         
 #checks and returns valid move possibilities, for a single piece for the player, all pieces for the AI
-def get_valid_moves(board, game, *args, **kwargs):
+def get_valid_moves(board, *args, **kwargs):
     chosen_piece = kwargs.get('chosen_piece', None)
     player_turn = kwargs.get('player_turn', False)
-    game = active_game
     if chosen_piece != None:
         posY = chosen_piece[0]
         posX = chosen_piece[1]
@@ -44,7 +46,7 @@ def get_valid_moves(board, game, *args, **kwargs):
     if game_is_finished():
         return
     else:
-        if active_game == 0:     
+        if game == 0:     
             if player_turn: #valid moves for single selected piece by player
                 if board[posY-1][posX] == 0:
                     board[posY-1][posX] = 3
@@ -56,7 +58,6 @@ def get_valid_moves(board, game, *args, **kwargs):
                         board[posY-1][posX+1] = 3
                 return board
             else: #all available valid moves
-                print("all possible moves for AI")
                 moves = []
                 for y,x in find_all(board, 2):
                     if board[y+1][x] == 0:
@@ -69,7 +70,7 @@ def get_valid_moves(board, game, *args, **kwargs):
                             moves.append([(y,x),(y+1,x+1)])
                 print(moves)
                 return moves
-        elif active_game == 1:
+        elif game == 1:
             if player_turn: #valid moves for single selected piece by player            
                 if posX >= 2:
                     if board[posY-1][posX-1] == 2 and board[posY-2][posX-2] == 0:
@@ -77,7 +78,7 @@ def get_valid_moves(board, game, *args, **kwargs):
                 if posX <= 3:
                     if board[posY-1][posX+1] == 2 and board[posY-2][posX+2] == 0:
                         board[posY-2][posX+2] = 3
-                if any('3' in row for row in board) == False:
+                if any(3 in row for row in board) == False:
                     if posX >= 1:
                         if board[posY-1][posX-1] == 0:
                             board[posY-1][posX-1] = 3
@@ -90,33 +91,31 @@ def get_valid_moves(board, game, *args, **kwargs):
                     print (board)
                     return board
             else: #all available valid moves
-                print("all possible moves for AI")
                 moves = []
                 for y,x in find_all(board, 2):                
                     if x >= 2:
                         if board[y+1][x-1] == 1 and board[y+2][x-2] == 0:
                             moves.append([(y,x),(y+2,x-2)])
-                    if x <= 4:
+                    if x <= 3:
                         if board[y+1][x+1] == 1 and board[y+2][x+2] == 0:
                             moves.append([(y,x),(y+2,x+2)])
-                    if any("3" in row for row in board) == False:
+                if moves == []:
+                    for y,x in find_all(board, 2):                
                         if x >= 1:
                             if board[y+1][x-1] == 0:
                                 moves.append([(y,x),(y+1,x-1)])
                         if x <= 4:
                             if board[y+1][x+1] == 0:
                                 moves.append([(y,x),(y+1,x+1)])
-                        print(moves)
-                        return moves
-                    else:
-                        print(moves)
-                        return moves
+                    return moves
+                else:
+                    return moves
         
 
 
 def switch_turn(board):
     # depending on what is sent, change board accordingly
-    ai.next_move(board, active_difficulty) #placeholder for communication with AI
+    ai.next_move(board, difficulty) #placeholder for communication with AI
     game_gui.player_start(board) #placeholder for communication to gui
 
 
