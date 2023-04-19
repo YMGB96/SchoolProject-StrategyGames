f4e92b151e0e09aa652266cae260637f7bfacf35
Nils Baumann <baumann@sipgate.de>
2023-04-19 15:57:38 +0200

    fill __main__.py

--- board_blitz/__main__.py
+++ board_blitz/__main__.py
@@ -1,6 +1,42 @@
+from game_gui import game_gui
+import menu_gui as mg
+from database import database
+import pygame as pg
+
+run = True
+
+def setup():
+    """Set everything up"""
+    pg.init()
+    mg.menu_gui = mg.MenuGui()
+
+def running():
+    """Handle every frame"""
+    # End the loop when the window is closed
+    if pg.event.get(pg.QUIT):
+        global run
+        run = False
+    # If there is a game_gui render that ...
+    if game_gui:
+        game_gui.render()
+    # ... render the menu if not
+    elif mg.menu_gui:
+        mg.menu_gui.render()
+    # Update Window
+    pg.display.update()
+    # Wait 50ms -> 20 fps
+    pg.time.wait(50)
+
+def clean_up():
+    """Close everything down"""
+    database.close()
+    pg.quit()
 
 def main():
-    pass
+    setup()
+    while run:
+        running()
+    clean_up()
 
 if __name__ == "__main__":
     main()
